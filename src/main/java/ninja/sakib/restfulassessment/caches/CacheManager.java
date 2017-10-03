94173d3fd725ddca1f07e8831f5b56d9868da652
Sakib Sami <s4kibs4mi@gmail.com>
2017-10-03 21:14:16 +0600

    caching implemented with redis

--- src/main/java/ninja/sakib/restfulassessment/caches/CacheManager.java
+++ src/main/java/ninja/sakib/restfulassessment/caches/CacheManager.java
@@ -1,5 +1,30 @@
 package ninja.sakib.restfulassessment.caches;
 
-public class CacheManager {
+import com.lambdaworks.redis.RedisClient;
+import com.lambdaworks.redis.api.StatefulRedisConnection;
+import com.lambdaworks.redis.api.sync.RedisStringCommands;
 
+public class CacheManager {
+    private static CacheManager cacheManager = new CacheManager();
+
+    public static CacheManager get() {
+        return cacheManager;
+    }
+
+    private String redisUri = "redis://128.199.95.1";
+    private RedisStringCommands commands;
+
+    private CacheManager() {
+        RedisClient redisClient = RedisClient.create(redisUri);
+        StatefulRedisConnection redisConnection = redisClient.connect();
+        commands = redisConnection.sync();
+    }
+
+    public void set(String key, String value, int timeInSeconds) {
+        commands.setex(key, timeInSeconds, value);
+    }
+
+    public String get(String key) {
+        return (String) commands.get(key);
+    }
 }
